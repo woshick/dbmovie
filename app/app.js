346a2ada7d59a3db5e6b224f0801f45eb04a29e7
woshick <fengjiezj@163.com>
2016-09-25 13:44:34 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -20,8 +20,8 @@ angular.module('moviecat', [
 ])
 .constant('appConfig', {
 	pageSize:10,
-	listApiAddress:"http://api.douban.com/v2/movie/",
-	detailApiAddress:"http://api.douban.com/v2/movie/subject/"
+	listApiAddress:"https://api.douban.com/v2/movie/",
+	detailApiAddress:"https://api.douban.com/v2/movie/subject/"
 })
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/in_theaters/1'});
